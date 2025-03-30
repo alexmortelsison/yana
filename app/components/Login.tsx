7c8789e32162be70b6e3279aafd43ba9a52c48c1
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-03-31 06:38:59 +0900

    added login

---
+++ app/components/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <button className="border px-8 py-2 text-white cursor-pointer hover:opacity-80">
+      Login
+    </button>
+  );
+}
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,27 @@
 import Image from "next/image";
 import Socials from "./Socials";
+import Link from "next/link";
+import Login from "./Login";
 
 export default function Navbar() {
   return (
     <div className="flex justify-between items-center max-w-[1600px] mx-auto pt-8 sticky top-0">
       <div className="flex space-x-16">
-        <Image
-          src={"/Yana.png"}
-          alt="logo"
-          width={500}
-          height={500}
-          className="object-cover w-50 h-25 invert"
-        />
+        <Link href="/">
+          <Image
+            src={"/Yana.png"}
+            alt="logo"
+            width={500}
+            height={500}
+            className="object-cover w-50 h-25 invert hover:opacity-90"
+          />
+        </Link>
         <Socials />
       </div>
       <div>B</div>
-      <div>C</div>
+      <div>
+        <Login />
+      </div>
     </div>
   );
 }
